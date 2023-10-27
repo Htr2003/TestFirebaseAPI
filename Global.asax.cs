5e37ed6f64ede785954f4065039e4dfe534fde22
KanRabpig <136266518+KanRabpig@users.noreply.github.com>
2023-10-27 14:32:05 +0700

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+
+namespace TestFirebaseAPI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            string credential = ("D://School//Nam 3//hk1//CNPMNC//TestFirebaseAPI//TestFirebaseAPI//adminSDK.json");
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credential);
+        }
+    }
+}
